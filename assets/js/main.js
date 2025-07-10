919297ed237f4adf7af0333b0fd9b410c397c0c5
asadbek <asadbekqulboyev@gmail.com>
2025-07-10 17:40:27 +0500

    desctop finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,40 @@
-$(window).scroll(function(){
-    $('.line').css({
-        transform:`translateY(${scrollY+10} + 'px')`
-        // height: scrollY/1000 + 'px'
+$(document).ready(function(){
+   
+    $('.product_swiper').each(function (index, element) {
+        // Har bir swiper'ga unikal class beramiz (masalan swiper-0, swiper-1, ...)
+        var swiperClass = 'swiper-' + index;
+        $(element).addClass(swiperClass);
+
+        // Ichidagi next/prev tugmalarini tanlab olamiz
+        var nextBtn = '.' + swiperClass + ' .swiper-button-next';
+        var prevBtn = '.' + swiperClass + ' .swiper-button-prev';
+
+        // Swiper'ni ishga tushiramiz
+        new Swiper('.' + swiperClass, {
+            loop: true,
+            navigation: {
+                nextEl: nextBtn,
+                prevEl: prevBtn,
+            }
+        });
+    });
+    // projects_slider
+    var projects_slider = new Swiper('.projects_slider', {
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            }
+         
+        },
+        
     })
-        var comunityTop = $('.comunity').offset().top;
-        var scrollTop = $(window).scrollTop();
-    
-        if (scrollTop >= comunityTop) {
-            $('.header').addClass('header_comunity');
-        } else {
-            $('.header').removeClass('header_comunity');
-        }
-    
-    
 })
